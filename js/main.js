6984f9533c7a016a30a68bf99c601420964bdc45
Thanatus <s.sidoryaka@gmail.com>
2020-11-12 18:12:44 +0200

    add test page 1-3

--- js/main.js
+++ js/main.js
@@ -4,30 +4,3 @@ var ctrl = new ScrollMagic.Controller({
     triggerHook: 'onLeave',
   },
 });
-
-// Create scene
-$('section').each(function () {
-  var name = $(this).attr('id');
-
-  new ScrollMagic.Scene({
-    triggerElement: this,
-  })
-    .setPin(this)
-    .addIndicators({
-      colorStart: 'rgba(255,255,255,0.5)',
-      colorEnd: 'rgba(255,255,255,0.5)',
-      colorTrigger: 'rgba(255,255,255,1)',
-      name: name,
-    })
-    .loglevel(3)
-    .addTo(ctrl);
-});
-
-// Get window height
-var wh = window.innerHeight;
-
-new ScrollMagic.Scene({
-  offset: wh * 3,
-})
-  .setClassToggle('section#four', 'is-active')
-  .addTo(ctrl);
