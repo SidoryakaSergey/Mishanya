f76d5e343e608b11aa49257d944f47096cad2800
thanatus <s.sidoryaka@gmail.com>
2020-11-30 02:27:30 +0200

    add hernya.html

--- js/jscript.js
+++ js/jscript.js
@@ -4,13 +4,14 @@ const bg = document.querySelector('.bg__page');
 
 window.addEventListener('scroll', function () {
   document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
-  if (pageYOffset > 200) {
+  console.log('scroll');
+  if (pageYOffset == 0) {
     console.log('1');
     bg.style.background = 'url(/img/test_p2.jpg) no-repeat';
     bg.style.backgroundSize = 'contain';
     bg.style.backgroundAttachment = 'fixed';
   }
-  if (pageYOffset < 200) {
+  if (pageYOffset == 200) {
     console.log('2');
     bg.style.background = 'url(/img/test_p1.jpg) no-repeat';
     bg.style.backgroundSize = 'contain';
