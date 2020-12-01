6e60bfb4da1637b666e3f2e61cab7913148077c2
Thanatus <s.sidoryaka@gmail.com>
2020-12-01 18:49:01 +0200

    add swipper

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-// Init ScrollMagic
-var ctrl = new ScrollMagic.Controller({
-  globalSceneOptions: {
-    triggerHook: 'onLeave',
+let pageSlider = new Swiper('.page', {
+  wrapperClass: 'page__wrapper',
+  slideClass: 'page__screen',
+
+  direction: 'vertical',
+  slidesPerView: 'auto',
+
+  parallax: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sesitivity: 1,
+  },
+  watchOverflow: true,
+  speed: 800,
+  observer: true,
+  observeParents: true,
+  observeSlideChuldren: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
   },
 });
